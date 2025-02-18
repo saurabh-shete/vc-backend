6b2cb00c5fcbb596b7e2d8d521b144b5ae49dd59
Saurabh Shete <ss.saurabhshete@gmail.com>
2025-02-18 20:08:41 +0530

    feat: add entry point to run FastAPI app with uvicorn in main.py

--- main.py
+++ main.py
@@ -23,3 +23,8 @@ app.include_router(linkedin.router)
 @app.get("/")
 def home():
     return {"message": "Welcome to LinkedIn Scraper API"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
